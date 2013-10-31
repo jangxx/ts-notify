7d54dafb16d5cb92d6e3141ec4f907fdf6635097
jangxx <admin@jangxx.com>
2013-10-31 21:47:53 +0100

    Fixed a bug where the script would reconnect 2^n times every 30 seconds

--- ts-notify.js
+++ ts-notify.js
@@ -7,7 +7,7 @@ var program = require('commander');
 program
 	.version("0.0.1")
 	.option("-p, --port <port>", "Server port [60000]", 60000)
-	.option("-i, --ip <ip>", "Remote ip address")
+	.option("-a, --address <address>", "Remote ip address/hostname")
 	.option("--file <file>", "Alternative 'friends' file")
 	.parse(process.argv);
 
@@ -15,7 +15,7 @@ if (!fs.existsSync(program.file || 'friends')) {
 	console.log('File "friends" missing');
 	process.exit(1);
 }
-if (!program.ip) {
+if (!program.address) {
 	program.help();
 }
 var friends = [];
@@ -28,10 +28,11 @@ if (friends.length < 1) {
 var subscriptionCount;
 var iconPath = fs.realpathSync('icon.png');
 resetVars();
+var r = new ReconnectManager();
 connect();
 
 function connect() {
-	var socket = net.connect(program.port, program.ip);
+	var socket = net.connect(program.port, program.address);
 	socket.setKeepAlive(true, 2*60*1000);
 	socket.on('data', function(data) {
 		if (data.toString().trim() == "ok" && subscriptionCount < friends.length) {
@@ -42,6 +43,7 @@ function connect() {
 		} else {
 			if (data.toString().trim() == 'ok') return;
 			var notification = JSON.parse(data.toString());
+			console.log(notification);
 			var action = (notification.status == 1) ? 'connected' : 'disconnected';
 			notify.icon(iconPath).notify('ts-notify', notification.name + ' ' + action);
 		}
@@ -49,18 +51,54 @@ function connect() {
 	socket.on('connect', function() {
 		console.log('Connected.');
 	});
-	socket.on('close', reconnect);
-	socket.on('error', reconnect);
+	socket.on('close', function() {
+		socket.destroy();
+		delete socket;
+		reconnect();
+	});
+	socket.on('error', function() {
+		socket.destroy();
+		delete socket;
+		reconnect();
+	});
+}
+
+function reconnect() {
+	var success = r.run(connect, 30000);
+	if (success) console.log("Lost connection to ts-notify-server. Reconnecting in 30 seconds.");
 }
 
 function resetVars() {
 	subscriptionCount = 0;
 }
 
-function reconnect() {
-	console.log('Disconnected. Attempting to reconnect in 30 seconds');
-	setTimeout(function() {
-		resetVars();
-		connect();
-	}, 30000);
+function ReconnectManager() {
+	var running = false;
+	var attempts = 0;
+	var lastAttempt;
+	this.run = function(fn, delay) {
+		if (running) return false;
+		var time = (new Date()).getTime();
+		if (lastAttempt) {
+			if (lastAttempt + 5*60*1000 > time) {
+				attempts++;
+			} else {
+				attempts = 1;
+			}
+			if(attempts > 3) {
+				console.log("Script exits after 3 failed attempts in 5 minutes");
+				notify.notify('ts-notify', 'The script exited after 3 failed attempts to reconnect.');
+				process.exit(1);
+			}
+		} else {
+			attempts = 1;
+		}
+		lastAttempt = time;
+		running = true;
+		setTimeout(function() {
+			fn();
+			running = false;
+		}, delay);
+		return true;
+	}
 }
